11a7792277b49aefbd8421e11cd1c3f3cb62ad6b
ngderli <derli_ng@hotmail.com>
2015-10-27 17:45:52 +0800

    setting

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Ukunili
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.initialize_on_precompile = false
   end
 end
