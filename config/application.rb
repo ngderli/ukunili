c533cd4bc6560bfe34e05fe90d07f1376c4b200d
ngderli <derli_ng@hotmail.com>
2015-11-03 16:09:55 +0800

    initialize respository

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -21,8 +22,9 @@ module Ukunili
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.initialize_on_precompile = false
   end
 end
